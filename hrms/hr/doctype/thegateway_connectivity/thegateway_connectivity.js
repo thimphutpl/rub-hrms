b06cad782e76d78b3ca41d741cb6f1d127d5cbdd
SangaTenzin <tenzinsanga1@gmail.com>
2026-02-11 12:17:25 +0600

    thegateway_connectivity

---
+++ hrms/hr/doctype/thegateway_connectivity/thegateway_connectivity.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("TheGateway Connectivity", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+frappe.ui.form.on('TheGateway Connectivity', {
+	refresh: function(frm) {
+
+	},
+	test_connectivity: function(frm){	
+		return frappe.call({
+			method: "hrms.hr.doctype.selected_candidate.selected_candidate.get_token",
+			callback: function (b) {
+				if (b.message) { 
+					frappe.msgprint("Connection successfull.")
+				}
+			},
+			freeze: true,
+			freeze_message: 'Connecting...'
+		});
+	}
+});
