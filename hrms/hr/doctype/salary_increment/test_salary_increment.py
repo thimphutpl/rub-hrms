3f2abac0c2abd69e655a7e4e1c5b7303172a1b76
Pemanorbu <pemanorbu@gmail.com>
2026-03-02 15:26:35 +0600

    journal Entry

--- hrms/hr/doctype/salary_increment/test_salary_increment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSalaryIncrement(FrappeTestCase):
-	pass
